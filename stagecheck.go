60bd1519931a6f23bda5be331c6163b25286a5da
princeantony70 <princeantony70@gmail.com>
2018-02-21 17:06:35 +0530

    completed working ApI

--- stagecheck.go
+++ stagecheck.go
@@ -4,47 +4,54 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
-
-	_ "github.com/go-sql-driver/mysql"
+_ "github.com/go-sql-driver/mysql"
 )
 
-var appdatabase *sql.DB
+
+var appdatabase  *sql.DB
 var err error
 var arr int
+
+
+type Tag struct {
+  ID   int    `json:"id"`
+
+}
+
 
 type Questions struct {
 	Question struct {
 		Name                 string `json:"name"`
-		Section              string `json:"section"`
-		Position             int    `json:"position"`
-		Title                string `json:"title"`
-		TitleSpanish         string `json:"titleSpanish"`
-		SubmitedValue        string `json:"submited_value"`
-		SpanishSubmitedValue string `json:"spanish_submited_value"`
-		Des                  string `json:"des"`
-		Ans                  string `json:"ans"`
-		ViewType             int    `json:"view_type"`
-		ParentID             int    `json:"parent_id"`
-		IsRequired           int    `json:"isRequired"`
-		IsSubmitField        int    `json:"is_submit_field"`
-		IsActive             int    `json:"is_active"`
+		Section              string      `json:"section"`
+		Position             int         `json:"position"`
+		Title                string      `json:"title"`
+		TitleSpanish         string      `json:"titleSpanish"`
+		SubmitedValue        string      `json:"submited_value"`
+		SpanishSubmitedValue string      `json:"spanish_submited_value"`
+		Des                  string      `json:"des"`
+		Ans                  string      `json:"ans"`
+		ViewType             int         `json:"view_type"`
+		ParentID             int         `json:"parent_id"`
+		IsRequired           int         `json:"isRequired"`
+		IsSubmitField        int         `json:"is_submit_field"`
+		IsActive             int         `json:"is_active"`
 	} `json:"question"`
 	Options []struct {
 		Name                 string `json:"name"`
-		Section              string `json:"section"`
-		Position             int    `json:"position"`
-		Title                string `json:"title"`
-		TitleSpanish         string `json:"titleSpanish"`
-		SubmitedValue        string `json:"submited_value"`
-		SpanishSubmitedValue string `json:"spanish_submited_value"`
-		Des                  string `json:"des"`
-		Ans                  string `json:"ans"`
-		ViewType             int    `json:"view_type"`
-		IsRequired           int    `json:"isRequired"`
-		IsSubmitField        int    `json:"is_submit_field"`
-		IsActive             int    `json:"is_active"`
+		Section              string      `json:"section"`
+		Position             int         `json:"position"`
+		Title                string      `json:"title"`
+		TitleSpanish         string      `json:"titleSpanish"`
+		SubmitedValue        string      `json:"submited_value"`
+		SpanishSubmitedValue string      `json:"spanish_submited_value"`
+		Des                  string      `json:"des"`
+		Ans                  string      `json:"ans"`
+		ViewType             int         `json:"view_type"`
+		IsRequired           int         `json:"isRequired"`
+		IsSubmitField        int         `json:"is_submit_field"`
+		IsActive             int         `json:"is_active"`
 	} `json:"options"`
 	Validation struct {
 		Messgae        string `json:"messgae"`
@@ -54,86 +61,41 @@ type Questions struct {
 	} `json:"validation"`
 }
 
-type Tag struct {
-	ID int `json:"id"`
-}
-
 func insertInDatabase(data Questions) error {
 
-	if len(data.Options) > 0 {
-		fmt.Println(data.Options)
-	} else {
-		fmt.Println(data.Question)
-	}
+if len(data.Options) > 0{
 
-	for i := 0; i <= 1; i++ {
-		switch i {
-		case 0:
-			_, err = appdatabase.Exec("INSERT INTO profile_questions(name, section, position,title,titleSpanish,submited_value,spanish_submited_value,des,ans,view_type,parent_id,isRequired,is_submit_field,is_active) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)", data.Question.Name, data.Question.Section, data.Question.Position, data.Question.Title, data.Question.TitleSpanish, data.Question.SubmitedValue, data.Question.SpanishSubmitedValue, data.Question.Des, data.Question.Ans, data.Question.ViewType, data.Question.ParentID, data.Question.IsRequired, data.Question.IsSubmitField, data.Question.IsActive)
-			results, err := appdatabase.Query("SELECT LAST_INSERT_ID()")
-			if err != nil {
-				panic(err.Error())
-			}
+_, err = appdatabase.Exec("INSERT INTO profile_questions(name, section, position,title,titleSpanish,submited_value,spanish_submited_value,des,ans,view_type,parent_id,isRequired,is_submit_field,is_active) VALUES(?, ?, ?,?,?,?,?,?,?,?,?,?,?,?)", data.Question.Name ,data.Question.Section ,data.Question.Position,data.Question.Title,data.Question.TitleSpanish,data.Question.SubmitedValue,data.Question.SpanishSubmitedValue,data.Question.Des,data.Question.Ans,data.Question.ViewType,data.Question.ParentID,data.Question.IsRequired,data.Question.IsSubmitField,data.Question.IsActive)
+}
 
-			for results.Next() {
-				var tag Tag
-				err = results.Scan(&tag.ID)
-				if err != nil {
-					panic(err.Error())
-				}
-				arr = tag.ID
-				fmt.Println(arr)
-			}
-
-		case 1:
-			for i := 0; i <= 1; i++ {
-				_, err = appdatabase.Exec("INSERT INTO profile_questions(name, section, position,title,titleSpanish,submited_value,spanish_submited_value,des,ans,view_type,parent_id,isRequired,is_submit_field,is_active) VALUES(?, ?, ?,?,?,?,?,?,?,?,?,?,?,?)", data.Options[i].Name, data.Options[i].Section, data.Options[i].Position, data.Options[i].Title, data.Options[i].TitleSpanish, data.Options[i].SubmitedValue, data.Options[i].SpanishSubmitedValue, data.Options[i].Des, data.Options[i].Ans, data.Options[i].ViewType, arr, data.Options[i].IsRequired, data.Options[i].IsSubmitField, data.Options[i].IsActive)
-
-			}
-		}
-	}
-
-	return err
+return err
 }
 
 func userAddHandler(w http.ResponseWriter, r *http.Request) {
-
-	//make byte array
-	out := make([]byte, 1024)
-	bodyLen, err := r.Body.Read(out)
-
-	if err != io.EOF {
-		fmt.Println(err.Error())
-		w.Write([]byte("{error:" + err.Error() + "}"))
-		return
-	}
-
-	var k Questions
-
-	err = json.Unmarshal(out[:bodyLen], &k)
-
-	if err != nil {
-		w.Write([]byte("{error:" + err.Error() + "}"))
-		return
-	}
-
+body, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+	fmt.Println("reading the Request error")
+    }
+var k Questions
+  err = json.Unmarshal(body, &k)
+  if err != nil {
+      fmt.Println("unmarshall error ")
+  }
 	err = insertInDatabase(k)
-
-	if err != nil {
-		w.Write([]byte("{error:" + err.Error() + "}"))
-		return
-	}
-
 	w.Write([]byte(`{"code ":"success"}`))
-	fmt.Println(k)
-
 }
 
+
 func init() {
-	appdatabase, err = sql.Open("mysql", "root:nfn@/api")
+fmt.Println("Go MySQL connection status :")
+appdatabase, err := sql.Open("mysql", "root:nfn@tcp(127.0.0.1:3306)/api")
+if err != nil {
+fmt.Println("db connection error")
+    }
+			    defer appdatabase.Close()
 }
 
 func main() {
 	http.HandleFunc("/add", userAddHandler)
-	http.ListenAndServe(":7082", nil)
+	http.ListenAndServe(":1221",nil)
 }
--- stick.go
+++ stick.go
@@ -1,13 +1,14 @@
-package main
+ package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
+       "encoding/json"
+       "net/http"
+       "fmt"
+       "io"
+      _ "github.com/go-sql-driver/mysql"
+       "database/sql"
 
-	_ "github.com/go-sql-driver/mysql"
+
 )
 
 var appdatabase *sql.DB
@@ -15,132 +16,121 @@ var err error
 var arr int
 
 type Tag struct {
-	ID int `json:"id"`
+  ID   int    `json:"id"`
 }
 
 type Questions struct {
-	Question struct {
+  Question struct {
+    Parent_id            int    `json:"parent_id"`
 		Name                 string `json:"name"`
 		Section              string `json:"section"`
-		Position             int    `json:"position"`
-		Title                string `json:"title"`
-		TitleSpanish         string `json:"titleSpanish"`
-		SubmitedValue        string `json:"submited_value"`
-		SpanishSubmitedValue string `json:"spanish_submited_value"`
-		Des                  string `json:"des"`
-		Ans                  string `json:"ans"`
-		ViewType             int    `json:"view_type"`
-		ParentID             int    `json:"parent_id"`
-		IsRequired           int    `json:"isRequired"`
-		IsSubmitField        int    `json:"is_submit_field"`
-		IsActive             int    `json:"is_active"`
+		Rollno               int    `json:"rollno"`
+		Age                  int    `json:"age"`
 	} `json:"question"`
 	Options []struct {
+    Parent_id            int    `json:"parent_id"`
 		Name                 string `json:"name"`
 		Section              string `json:"section"`
-		Position             int    `json:"position"`
-		Title                string `json:"title"`
-		TitleSpanish         string `json:"titleSpanish"`
-		SubmitedValue        string `json:"submited_value"`
-		SpanishSubmitedValue string `json:"spanish_submited_value"`
-		Des                  string `json:"des"`
-		Ans                  string `json:"ans"`
-		ViewType             int    `json:"view_type"`
-		IsRequired           int    `json:"isRequired"`
-		IsSubmitField        int    `json:"is_submit_field"`
-		IsActive             int    `json:"is_active"`
+		Rollno               int    `json:"rollno"`
+		Age                  int    `json:"age "`
+
 	} `json:"options"`
-	Validation struct {
-		Messgae        string `json:"messgae"`
-		MessageSpanish string `json:"messageSpanish"`
-		Regx           string `json:"regx"`
-		Format         string `json:"format"`
-	} `json:"validation"`
 }
 
-func insertInDatabase(data Questions) error {
 
-	if len(data.Options) > 0 {
-		fmt.Println(data.Options)
-	} else {
-		fmt.Println(data.Question)
-	}
 
-	for i := 0; i <= 1; i++ {
-		switch i {
-		case 0:
-			_, err = appdatabase.Exec("INSERT INTO profile_questions(name, section, position,title,titleSpanish,submited_value,spanish_submited_value,des,ans,view_type,parent_id,isRequired,is_submit_field,is_active) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)", data.Question.Name, data.Question.Section, data.Question.Position, data.Question.Title, data.Question.TitleSpanish, data.Question.SubmitedValue, data.Question.SpanishSubmitedValue, data.Question.Des, data.Question.Ans, data.Question.ViewType, data.Question.ParentID, data.Question.IsRequired, data.Question.IsSubmitField, data.Question.IsActive)
-			results, err := appdatabase.Query("SELECT LAST_INSERT_ID()")
-			if err != nil {
-				panic(err.Error())
-			}
+func insertInDatabase(data Questions) error  {
 
-			for results.Next() {
-				var tag Tag
-				err = results.Scan(&tag.ID)
-				if err != nil {
-					panic(err.Error())
-				}
-				arr = tag.ID
-				fmt.Println(arr)
-			}
 
-		case 1:
-			for i := 0; i <= 1; i++ {
-				_, err = appdatabase.Exec("INSERT INTO profile_questions(name, section, position,title,titleSpanish,submited_value,spanish_submited_value,des,ans,view_type,parent_id,isRequired,is_submit_field,is_active) VALUES(?, ?, ?,?,?,?,?,?,?,?,?,?,?,?)", data.Options[i].Name, data.Options[i].Section, data.Options[i].Position, data.Options[i].Title, data.Options[i].TitleSpanish, data.Options[i].SubmitedValue, data.Options[i].SpanishSubmitedValue, data.Options[i].Des, data.Options[i].Ans, data.Options[i].ViewType, arr, data.Options[i].IsRequired, data.Options[i].IsSubmitField, data.Options[i].IsActive)
+  if len(data.Options) > 0{
+    fmt.Println(data.Options)
+  }else{
+    fmt.Println(data.Question)
+  }
 
-			}
-		}
-	}
+  for i:=0;i<=1;i++{
+      switch i {
+      case 0:
+      _, err = appdatabase.Exec("INSERT INTO array(parent_id,name, section, rollno,age) VALUES(?,?,?,?,?)",data.Question.Parent_id,data.Question.Name,data.Question.Section , data.Question.Rollno,data.Question.Age)
+      results, err := appdatabase.Query("SELECT LAST_INSERT_ID()")
+      if err != nil {
+       panic(err.Error()) // proper error handling instead of panic in your app
+     	}
 
-	return err
+      for results.Next() {
+      var tag Tag
+       	// for each row, scan the result into our tag composite object
+       err = results.Scan(&tag.ID)
+       if err != nil {
+       panic(err.Error()) // proper error handling instead of panic in your app
+       }
+      // and then print out the tag's Name attribute
+      arr=tag.ID
+      fmt.Println(arr)
+     	 }
+
+      case 1:
+      for i:=0;i<=1;i++{
+      _, err = appdatabase.Exec("INSERT INTO array(parent_id,name, section, rollno,age) VALUES(?,?,?,?,?)",arr,data.Options[i].Name ,data.Options[i].Section,data.Options[i].Rollno,data.Options[i].Age)
+
+   }
+ }
 }
+
+return err
+}
+
+
 
 func userAddHandler(w http.ResponseWriter, r *http.Request) {
 
-	//make byte array
-	out := make([]byte, 1024)
-	bodyLen, err := r.Body.Read(out)
 
-	if err != io.EOF {
-		fmt.Println(err.Error())
-		w.Write([]byte("{error:" + err.Error() + "}"))
-		return
-	}
+       //make byte array
+       out := make([]byte,1024)
+       bodyLen, err := r.Body.Read(out)
 
-	var k Questions
+       if err != io.EOF {
+              fmt.Println(err.Error())
+              w.Write([]byte("{error:" + err.Error() + "}"))
+              return
+       }
 
-	err = json.Unmarshal(out[:bodyLen], &k)
+       var k Questions
 
-	if err != nil {
-		w.Write([]byte("{error:" + err.Error() + "}"))
-		return
-	}
+       err = json.Unmarshal(out[:bodyLen],&k)
 
-	err = insertInDatabase(k)
 
-	if err != nil {
-		w.Write([]byte("{error:" + err.Error() + "}"))
-		return
-	}
+       if err != nil {
+              w.Write([]byte("{error:" + err.Error() + "}"))
+              return
+       }
 
-	w.Write([]byte(`{"code ":"success"}`))
-	fmt.Println(k)
+       err = insertInDatabase(k)
+
+       if err != nil {
+              w.Write([]byte("{error:" + err.Error() + "}"))
+              return
+       }
+
+    w.Write([]byte(`{"code ":"success"}`))
+    fmt.Println(k)
 
 }
 
-func init() {
-	appdatabase, err = sql.Open("mysql", "root:nfn@/api")
-	if err != nil {
-		fmt.Println("db error ")
-	}
-	err = appdatabase.Ping()
-	if err != nil {
-		fmt.Println("ping error")
-	}
+
+func init(){
+appdatabase, err = sql.Open("mysql", "root:nfn@/json")
+if err != nil{
+  fmt.Println("db error ")
 }
+err = appdatabase.Ping()
+if err !=nil{
+  fmt.Println("ping error")
+}
+}
+
 
 func main() {
-	http.HandleFunc("/add", userAddHandler)
-	http.ListenAndServe(":7077", nil)
+       http.HandleFunc("/add", userAddHandler)
+       http.ListenAndServe(":2221", nil)
 }
